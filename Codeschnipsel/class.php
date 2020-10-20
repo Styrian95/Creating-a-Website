78c0337afd63a54d00c00e67d37e1817c9565f52
Styrian95 <73189166+Styrian95@users.noreply.github.com>
2020-10-20 18:49:26 +0200

    Add files via upload

---
+++ Codeschnipsel/class.php
@@ -0,0 +1,20 @@
+<?php
+
+// Definition of the user class
+class User {
+	// represents the database table
+	public $userId;
+	public $email;
+	public $password;
+	public $firstname;
+	public $lastname;
+	public $postalCode;
+	public $city;
+	public $street;
+	public $courseId;
+	public $createdAt;
+
+}
+
+
+?>
