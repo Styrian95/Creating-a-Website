f3da5efeb766c011c8911202524282fbc83ce57d
Styrian95 <73189166+Styrian95@users.noreply.github.com>
2020-10-20 20:38:11 +0200

    Update Login.php

--- Codeschnipsel/Login.php
+++ Codeschnipsel/Login.php
@@ -1,3 +1,5 @@
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>PHP
+
 <?php
 session_start();
 require_once('../modules/config.php');
@@ -23,65 +25,68 @@ if (isset($_GET['login'])) {
 }
 ?>
 
+>>>>>>>>>>>>>>>>>>>>>>HTML
+<div style=" display: block;margin-left: auto;margin-right: auto;width: max-content;">
+	<form action="?login=1" method="post">
+		E-Mail:<br>
+		<input type="email" size="40" maxlength="250" name="email"><br><br>
+		Dein Passwort:<br>
+		<input type="password" size="40" maxlength="250" name="password"><br><br>
+		<input type="submit" value="Abschicken">
+	</form>
+	<br>
+	<?php
+	// Displays an error message on the top of the site if one is set 
+	if (isset($errorMessage)) {
+		echo $errorMessage;
+	}
+	?>
+</div>
 
-<!DOCTYPE html>
-<html lang="en">
+>>>>>>>>>>>>>>>>>>>>SQL
+    // Determines if a user login attempt succeeds or fails
+    public function login($email, $password) {
+        // Checks if the user is in the database
+        $user = $this->getUser($email);
+        // Cecks if a user got returned
+        if ($user) {
+            // Checks if the provides password matches
+            if (password_verify($password, $user->password)) {
+                // Returns the user
+                return $user;
+            } else {
+                // Password didn't match
+                return false;
+            }
+        } else {
+            // email is not in the database
+            return false;
+        }
+    }
 
-<head>
-	<title>User Login</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="../src/css/navbar.css">
-	<link rel="stylesheet" href="../src/css/styles.css">
-</head>
+    public function getUser($email) {
+        // Prepare db-statements
+        $sql = "SELECT * FROM t_user WHERE email =?";
+        $stmt= $this->mysqli->prepare($sql);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
 
-<body>
-
-	<!--navbar-->
-	<div id="navbar">
-		<ul>
-		  <li><a  href="../index.html">Startseite</a></li>
-		  <li><a class="active" href="userlogin.php">Login</a></li>
-		  <li style="float:right"><a href="internallogin.php">Login Internal</a></li>
-		</ul>
-	</div>
-
-
-
-	<!--content-->
-	<div id="content">
-		<h2>User Login</h2>
-		    <!-- Div that centers the displayed login form -->
-	    <div style=" display: block;margin-left: auto;margin-right: auto;width: max-content;">
-	        <form action="?login=1" method="post">
-	            E-Mail:<br>
-	            <input type="email" size="40" maxlength="250" name="email"><br><br>
-	            Dein Passwort:<br>
-	            <input type="password" size="40" maxlength="250" name="password"><br><br>
-	            <input type="submit" value="Abschicken">
-	        </form>
-
-	    
-	        <br>
-		<?php
-	    // Displays an error message on the top of the site if one is set 
-	    if (isset($errorMessage)) {
-	        echo $errorMessage;
-	    }
-	    ?>
-	    </div>
-
-	</div>
-
-
-
-
-	<!--footer-->
-	<footer>
-		<p>LAP (C) Zwatz</p>
-	</footer>
-
-</body>
-
-
-</html>
+        $sqlResult = $stmt->get_result()->fetch_assoc(); 
+        if (is_countable($sqlResult) && count($sqlResult) > 0) {
+            // Creates an new user
+            $user = new User();
+            $user->userId = $sqlResult['user_id'];
+            $user->courseId = $sqlResult['course_id'];
+            $user->email = $sqlResult['email'];
+            $user->password = $sqlResult['password'];
+            $user->firstname = $sqlResult['firstname'];
+            $user->lastname = $sqlResult['lastname'];
+            $user->postalCode = $sqlResult['postal_code'];
+            $user->city = $sqlResult['city'];
+            $user->street = $sqlResult['street'];
+        } else{
+            return false;
+        }
+        // Returns the created user
+        return $user;
+    }
